9a53d6cc1be0d258771cff6af559480a5684343c
ShauliAlgawi <shauli84@gmail.com>
2025-02-15 22:49:52 +0200

    first commit

---
+++ AlergyScanner/AlergyScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlergyScannerApp.swift
+//  AlergyScanner
+//
+//  Created by Shauli Algawi on 15/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct AlergyScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
